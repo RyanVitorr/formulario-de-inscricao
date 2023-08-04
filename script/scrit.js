221ad5590b04da78c215bb15a4f44015820718f3
Ryan  Vitor <ryanvitor29@gmail.com>
2023-08-03 21:07:07 -0300

    atualizei o banner

--- script/scrit.js
+++ script/scrit.js
@@ -18,3 +18,29 @@ function mudarPaginas() {
 }
 
 botaoTrocaTela.addEventListener('click', mudarPaginas);
+
+const botaoBanner = document.getElementById('botao-banner');
+const banner = document.getElementById('banner');
+
+const h1Banner = document.getElementById('h1-banner');
+const pBanner = document.getElementById('p-banner');
+
+function moverBanner() {
+    if (banner.style.left == "") {
+        banner.style.left = "24%";
+        banner.style.borderRadius = "10px 0 0 10px";
+        banner.style.transition =  "1s";
+        h1Banner.innerText = 'Bem vindo(a) de volta!';
+        h1Banner.style.fontSize = '1.5rem';
+        pBanner.innerText = 'Para se manter conectado conosco, faça login com seus dados pessoais';
+    } else {
+        banner.style.left = "";
+        h1Banner.innerText = 'Olá!';
+        h1Banner.style.fontSize = '2rem';
+        pBanner.innerText = 'Insira seus dados pessoas para abrir uma conta conosco';
+        banner.style.borderRadius = "0 10px 10px 0";
+        banner.style.transition =  "1s";
+    }
+}
+
+botaoBanner.addEventListener('click', moverBanner);
