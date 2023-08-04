6b41604c2b707c9dc2ce2f8aa2e94fe811bb2296
Ryan  Vitor <ryanvitor29@gmail.com>
2023-08-04 11:53:15 -0300

    finalizei o projeto

--- script/scrit.js
+++ script/scrit.js
@@ -24,21 +24,24 @@ const banner = document.getElementById('banner');
 
 const h1Banner = document.getElementById('h1-banner');
 const pBanner = document.getElementById('p-banner');
+const valuebotao = document.getElementById('botao-banner');
 
 function moverBanner() {
     if (banner.style.left == "") {
         banner.style.left = "24%";
         banner.style.borderRadius = "10px 0 0 10px";
-        banner.style.transition =  "1s";
         h1Banner.innerText = 'Bem vindo(a) de volta!';
         h1Banner.style.fontSize = '1.5rem';
         pBanner.innerText = 'Para se manter conectado conosco, faça login com seus dados pessoais';
+        valuebotao.value = 'SING IN';
+        banner.style.transition =  "1s";
     } else {
         banner.style.left = "";
         h1Banner.innerText = 'Olá!';
         h1Banner.style.fontSize = '2rem';
         pBanner.innerText = 'Insira seus dados pessoas para abrir uma conta conosco';
         banner.style.borderRadius = "0 10px 10px 0";
+        valuebotao.value = 'SING up';
         banner.style.transition =  "1s";
     }
 }
